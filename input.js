f1554a97644c3b05d04178079075232aaec6ef14
laysejuju <laysejuju@gmail.com>
2015-07-07 20:58:03 -0300

    Pequena alterações

--- input.js
+++ input.js
@@ -3,11 +3,10 @@ function onPlay(){
 	document.getElementById("p1").style.left = p + "px";
 	document.getElementById("start").style.display = 'none';
 	
-		//setInterval(gameLoop, 60);
+		setInterval(gameLoop, 60);
 	
 	
 }
-
 var denteX = 10;
 var denteY =-10;
 var velocidade=10;
@@ -16,9 +15,20 @@ var p = 250;
 function gameLoop(){
 	document.getElementById("dente").style.top = denteY + "px";
 	
-	for (var i = 0; i < 10; i++) { 
+//	for (var i = 0; i < 10; i++) { 
 		//document.getElementById("cenario").innerHTML="<div id='dente' class='objeto'></div>";
+//	}
+	if(document.getElementById("dente").style.top >= "0px"){
+		document.getElementById("dente").style.top = velocidade + denteY + "px";
+		console.log("Desce!");
 	}
+	if(document.getElementById("dente").style.top == document.getElementById("piso").style.height){
+		document.getElementById("dente").style.top = denteY + "px";
+		console.log("Passou do tamanho");
+	}
+	
+
+	
 		denteY+=velocidade;
 }
 
@@ -26,7 +36,6 @@ function onLeft(){
 	p-=10;
 	document.getElementById("p1").style.left = p + "px";
 	console.log("Left!");
-	
 }
 
 function onRight(){
