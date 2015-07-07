6f53133fc47ef45614d033e796a9172068eb7025
laysejuju <laysejuju@gmail.com>
2015-07-07 17:14:40 -0300

    Alterações

--- input.js
+++ input.js
@@ -2,9 +2,25 @@ function onPlay(){
 	var p = 250;
 	document.getElementById("p1").style.left = p + "px";
 	document.getElementById("start").style.display = 'none';
+	
+		//setInterval(gameLoop, 60);
+	
+	
 }
 
+var denteX = 10;
+var denteY =-10;
+var velocidade=10;
 var p = 250;
+
+function gameLoop(){
+	document.getElementById("dente").style.top = denteY + "px";
+	
+	for (var i = 0; i < 10; i++) { 
+		//document.getElementById("cenario").innerHTML="<div id='dente' class='objeto'></div>";
+	}
+		denteY+=velocidade;
+}
 
 function onLeft(){
 	p-=10;
@@ -18,3 +34,21 @@ function onRight(){
 	document.getElementById("p1").style.left = p + "px";
 	console.log("Right!");
 }
+
+/*
+
+function jogar(){
+	iniciar = setInterval(gameLoop, 30);
+}
+
+if(bolaPosY <= canvas.height)
+				{
+					bolaPosY += velocidadeBola;
+				}
+				else
+				{
+					bolaPosX = Math.random() * 600;
+					bolaPosY = -10;
+					colisao = false;
+				}
+*/
