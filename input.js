7608b0853f99059f29d65e1922ed20e9bfa8d773
laysejuju <laysejuju@gmail.com>
2015-07-08 15:09:51 -0300

    Atualização

--- input.js
+++ input.js
@@ -3,21 +3,33 @@ function onPlay(){
 	document.getElementById("p1").style.left = p + "px";
 	document.getElementById("start").style.display = 'none';
 	
-		setInterval(gameLoop, 60);
+	var pisoY = 0;
+	document.getElementById("piso").style.bottom = pisoY + "px";
 	
+	jogar();
+	
+
 	
 }
-var denteX = 10;
+
 var denteY =-10;
 var velocidade=10;
 var p = 250;
+var pisoY = 0;
 
-function gameLoop(){
-	document.getElementById("dente").style.top = denteY + "px";
+function jogar(){
+	var denteX = Math.random() * 600;
+	setInterval(gameLoop(denteX),120);
+}
+
+function gameLoop(denteX){
+	//document.getElementById("dente").style.top = denteY + "px";
 	
 //	for (var i = 0; i < 10; i++) { 
 		//document.getElementById("cenario").innerHTML="<div id='dente' class='objeto'></div>";
 //	}
+
+/*
 	if(document.getElementById("dente").style.top >= "0px"){
 		document.getElementById("dente").style.top = velocidade + denteY + "px";
 		console.log("Desce!");
@@ -26,10 +38,24 @@ function gameLoop(){
 		document.getElementById("dente").style.top = denteY + "px";
 		console.log("Passou do tamanho");
 	}
-	
 
-	
+*/
+
+
+		
 		denteY+=velocidade;
+		//console.log(velocidade++);
+		//for (var i=0; i<10; i++){
+			var dentePosX = document.getElementById('dente');
+			dentePosX.style.left = parseInt(denteX) + "px" ;
+			var dentePosY = document.getElementById('dente');
+			dentePosY.style.top = parseInt(denteY) + "px" ;
+			if(denteY > 300){
+				dentePosY.style.top = denteY=0;
+			}
+			console.log(denteX);
+		//}
+
 }
 
 function onLeft(){
